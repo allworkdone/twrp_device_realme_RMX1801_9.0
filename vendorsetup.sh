e5fe27c841949d71a1e9c4150c6febd3cc5b83f0
allworkdone <raj635321@gmail.com>
2021-09-19 12:46:51 +0530

    RMX1801:vendorsetup.sh: Setup BuildVars

---
+++ vendorsetup.sh
@@ -0,0 +1,55 @@
+# Set Fox version
+export FOX_VERSION="R11"
+export FOX_R11=1
+export FOX_BUILD_TYPE="Beta"
+
+# Allow missing dependencies
+export ALLOW_MISSING_DEPENDENCIES=true
+
+# Platform Info
+export PLATFORM_VERSION="16.1.0"
+export PLATFORM_SECURITY_PATCH="2099-12-31"
+
+# Build Nano editor
+export FOX_USE_NANO_EDITOR=1
+
+# Use TWRP Recovery Image Build Tools
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+
+#Set locale to C
+export LC_ALL="C"
+
+# Set maintainer name
+export OF_MAINTAINER="allworkdone"
+
+# Set maintainer Avatar
+#export OF_MAINTAINER_AVATAR="/home/allworkdone/Downloads/avatar.png"
+
+# App Manager
+export FOX_DISABLE_APP_MANAGER=1
+
+# Display Size
+export OF_STATUS_H=95
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_SCREEN_H=2340
+
+# Disable/Enable
+export OF_HIDE_NOTCH=1
+
+# Clock Position
+export OF_CLOCK_POS=1
+
+# Patch AVB2.0
+export OF_PATCH_AVB20=1
+
+# Auto SAR detect
+export OF_USE_TWRP_SAR_DETECT=1
+
+# Quick Backup Entries
+export OF_QUICK_BACKUP_LIST="/data;/boot;"
+
+#export FOX_USE_SPECIFIC_MAGISK_ZIP=/home/deepak/source/fox/Magisk-v23.0.zip
+
+# Enable Button unlock instead of swipe
+export OF_USE_LOCKSCREEN_BUTTON=1
